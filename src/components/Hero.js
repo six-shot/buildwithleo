9687509e496fd5032ef814f19f7568172b5e7296
six-shot <okhuomonajayi54@gmail.com>
2024-01-18 11:26:08 +0100

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
         <div>
           <Header />
           <div className="flex justify-center h-[50vh] items-center">
-            <div className="flex sm:gap-20 justify-between sm:justify-center items-center w-full px-[10%] sm:px-0  uppercase font-bold text-[32px] text-white">
+            <div className="flex sm:gap-20 justify-between sm:justify-center items-center w-full px-[7%] sm:px-0  uppercase font-bold sm:text-[32px] text-[20px] text-white">
               <Idea />
               <People />
               <Technology />
