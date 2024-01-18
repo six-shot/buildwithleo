18d0dadc5b53956e3fff898be26c7dcba17453a7
six-shot <okhuomonajayi54@gmail.com>
2024-01-18 03:31:52 +0100

    fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,15 +9,14 @@ const menu = {
     open: {
         width: "480px",
         height: "50vh",
-        top: "-25px",
-        right: "-25px",
+       
+        
         transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
     },
     closed: {
         width: "100px",
         height: "40px",
-        top: "0px",
-        right: "0px",
+        
         transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1]}
     }
 }
@@ -26,8 +25,10 @@ export default function Index() {
     const [isActive, setIsActive] = useState(false);
 
     return (
-      <div className='flex justify-between items-center w-full relative'>
-        hhh
+      <div className="flex justify-between items-center w-full h-[80px] relative">
+        <h5 className="uppercase text-white text-[24px] font-bold ">
+          Workwithleo
+        </h5>
         <div className={styles.header}>
           <motion.div
             className={styles.menu}
