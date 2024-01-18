ec6fbdb180a53ddd29b53cefd18a39161560f32f
six-shot <okhuomonajayi54@gmail.com>
2024-01-18 11:40:05 +0100

    fix

--- src/components/WhyWithUs.js
+++ src/components/WhyWithUs.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
 
 export default function WhyWithUs() {
   return (
-    <div className='px-[5%]'>WhyWithUs</div>
-  )
+    <div className="px-[5%] bg-[#000021]">
+      <h4 className="text-white text-center text-[24px] pt-[3%]">
+        Why Work with us
+      </h4>
+      <div className="grid sm:grid-cols-3 sm:gap-x-24 sm:gap-y-24 gap-8 mt-6 sm:mt-32">
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+        <div className="text-white ">
+          <h5 className="text-[24px] font-bold mb-3">Resilence</h5>
+          <h6 className=" text-sm">
+            At our core, we believe that a strong brand is the heartbeat of ay
+            sucessful eterprice. It is the essence that captures attentio,builds
+            trust and forges lasting conection with customers
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 }
