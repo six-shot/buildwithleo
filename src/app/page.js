71f7caceb1c02f762663f5fe2f95769a6e446d17
six-shot <okhuomonajayi54@gmail.com>
2024-01-20 13:43:35 +0100

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,30 @@
 import AboutUs from '@/components/AboutUs'
 import Hero from '@/components/Hero'
 import WhyWithUs from '@/components/WhyWithUs';
-import ZoomParallax from "../components/ZoomParallax/index";
 
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+
+// import { useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
+import SlidingImages from "../components/SlidingImages";
 
 export default function Home() {
-   useEffect(() => {
-     const lenis = new Lenis();
+  //  useEffect(() => {
+  //    const lenis = new Lenis();
 
-     function raf(time) {
-       lenis.raf(time);
-       requestAnimationFrame(raf);
-     }
+  //    function raf(time) {
+  //      lenis.raf(time);
+  //      requestAnimationFrame(raf);
+  //    }
 
-     requestAnimationFrame(raf);
-   }, []);
+  //    requestAnimationFrame(raf);
+  //  }, []);
   return (
     <main className="max-w-[1440px] mx-auto">
       <Hero />
       <AboutUs />
       <WhyWithUs/>
-      <ZoomParallax/>
+
+      <SlidingImages />
     </main>
   );
 }
